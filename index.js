10061002255c18bb728d000fb7b05ff8833772b8
Priyanka <priyankadavara@gmail.com>
2023-03-24 15:34:41 +0100

    intermediate commit to merge

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const express = require("express");
 const bodyParser = require('body-parser')
 const dotenv = require("dotenv");
 dotenv.config();
+const cloudinary = require("cloudinary").v2;
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+
+// Set up Cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 // DATABASE
-const app = express();
 const PORT = process.env.PORT || 8000;
 const { Pool } = require("pg");
 const pool = new Pool({
@@ -14,7 +25,7 @@ const cors = require("cors");
 const usersRouter = require('./routes/routeUsers')
 const propertyRouter = require('./routes/routeProperty')
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
         extended: true,
@@ -26,18 +37,12 @@ app.use(cors());
 app.use("/api/users", usersRouter);
 app.use("/api/property", propertyRouter);
 
-
 // TEST index
-app.get('/', (request, response) => {
-    response.json({
-        info: 'Node.js, Express, and Postgres API'
-    })
-})
-
-
-
-
-
+app.get("/", (request, response) => {
+  response.json({
+    info: "Node.js, Express, and Postgres API",
+  });
+});
 
 // app.get("/api/users", (req, res) => {
 //     pool
